1d81dbe17e2cf0d8dd6b41c632e7c76b1e6a54ee
Thijs Alkemade <me@thijsalkema.de>
2013-10-16 00:33:53 +0200

    Forgot to add common.php to pem.php.

--- pem.php
+++ pem.php
@@ -2,9 +2,9 @@
 
 $digest = $_GET['sha256'];
 
-if (isset($digest)) {
-	$dbconn = pg_connect("port=5433 host=localhost dbname=xmppoke user=xmppoke password=xmppoke") or die('Could not connect: ' . pg_last_error());
+include("common.php");
 
+if (isset($digest)) {
 	pg_prepare($dbconn, "find_cert", "SELECT pem FROM certificates WHERE digest_sha256 = $1 LIMIT 1;");
 
 	$res = pg_execute($dbconn, "find_cert", array($digest));
