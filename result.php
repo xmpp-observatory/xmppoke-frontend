62ba25e99ecdc3bd587e97ad06ff686548a028b4
Thijs Alkemade <me@thijsalkema.de>
2013-10-21 21:32:57 +0200

    Fix some comparisions to check the type.

--- result.php
+++ result.php
@@ -196,21 +196,21 @@ foreach ($subjects as $subject) {
 		<h5>Details</h5>
 
 <?php
-if ($cert["trusted_root"] === 't' && $cert["chain_index"]) {
+if ($cert["trusted_root"] === 't' && $cert["chain_index"] !== NULL) {
 ?>
 		<div class="alert alert-block alert-warning">
 			<strong>Warning:</strong> Trusted root certificate is included in the chain.
 		</div>
 <?php
 }
-if ($cert["trusted_root"] === 'f' && $cert["chain_index"] == NULL) {
+if ($cert["trusted_root"] === 'f' && $cert["chain_index"] === NULL) {
 ?>
 		<div class="alert alert-block alert-danger">
 			<strong>Error:</strong> Intermediate certificate was not included in the chain.
 		</div>
 <?php
 }
-if ($prev_signed_by_id != $cert["certificate_id"] && $cert["chain_index"] != 0) {
+if ($prev_signed_by_id !== $cert["certificate_id"] && $cert["chain_index"] !== 0) {
 ?>
 		<div class="alert alert-block alert-warning">
 			<strong>Warning:</strong> Certificate is unused.
