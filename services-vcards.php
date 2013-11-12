62c93c70c9a31d0ac2affa0056e98f91a5d62bdd
Thijs Alkemade <me@thijsalkema.de>
2013-11-12 12:23:42 +0100

    This was wrong, we need the issuer, not the next certificate in the chain (as that can be manipulated).

--- services-vcards.php
+++ services-vcards.php
@@ -12,12 +12,12 @@ $res = pg_execute($dbconn, "list_server", array());
 
 $list = pg_fetch_all($res);
 
-pg_prepare($dbconn, "find_cn", "SELECT value FROM srv_results, srv_certificates, certificate_subjects WHERE test_id = (SELECT test_id FROM test_results WHERE server_name = $1 ORDER BY test_date DESC LIMIT 1) AND srv_results.srv_result_id = srv_certificates.srv_result_id AND srv_certificates.certificate_id = certificate_subjects.certificate_id AND chain_index = $2 AND certificate_subjects.name = 'commonName';");
+pg_prepare($dbconn, "find_cn", "SELECT value FROM srv_results, srv_certificates, certificate_subjects, certificates, certificates as issuers WHERE test_id = (SELECT test_id FROM test_results WHERE server_name = $1 ORDER BY test_date DESC LIMIT 1) AND srv_results.srv_result_id = srv_certificates.srv_result_id AND issuers.certificate_id = certificate_subjects.certificate_id AND chain_index = 0 AND certificate_subjects.name = 'commonName' AND certificates.certificate_id = srv_certificates.certificate_id AND chain_index = '0' AND certificates.signed_by_id = issuers.certificate_id;");
 
 pg_prepare($dbconn, "find_version", "SELECT version FROM test_results WHERE server_name = $1 ORDER BY test_date DESC LIMIT 1;");
 
 foreach ($list as $result) {
-	$res = pg_execute($dbconn, "find_cn", array($result["server_name"], 1));
+	$res = pg_execute($dbconn, "find_cn", array($result["server_name"]));
 
 	$cn = pg_fetch_assoc($res);
 
