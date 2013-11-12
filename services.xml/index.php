2e45327479254a3d0a6f422231e4bd12cb8ed5b9
Thijs Alkemade <me@thijsalkema.de>
2013-11-12 11:10:42 +0100

    Added services.xml back.

---
+++ services.xml/index.php
@@ -0,0 +1,20 @@
+<?xml version="1.0"?>
+<query xmlns:reg="urn:xmpp:vcard:registration:1">
+<?php
+
+include("../common.php");
+
+pg_prepare($dbconn, "list_server", "SELECT server_name FROM public_servers ORDER BY server_name;");
+
+$res = pg_execute($dbconn, "list_server", array());
+
+$list = pg_fetch_all($res);
+
+foreach ($list as $result) {
+?>
+	<item jid='<?= $res["server_name"] ?>'/>
+<?php
+}
+
+?>
+</query>
