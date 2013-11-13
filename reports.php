465d73b4204d21d50d317556f14daa35160e1793
Thijs Alkemade <me@thijsalkema.de>
2013-11-13 19:15:48 +0100

    The stats page is getting so long, I want a menu here.

--- reports.php
+++ reports.php
@@ -174,97 +174,109 @@ common_header();
 	</div>
 
 	<div class="container">
+		<div class="row">
 
-		<h1>Various reports of all servers tested</h1>
+			<div class="col-md-3">
+				<div id="sidebar" class="side-bar" role="complementary">
+					<ul class="nav">
+						<li class="active"><a href="#tls">TLS versions</a></li>
+						<li><a href="#grades">Grades</a></li>
+					</ul>
+				</div>
+			</div>
 
-        <h3 id="tls">TLS versions <small class="text-muted"><?= $total["count"] ?> results</small></h3>
+			<div class="col-md-9">
 
-		<table class="table table-bordered table-striped">
-			<tr>
-                <td>SSL 2</td>
-                <td><?= count($sslv2) ?> <span class="text-muted"><?= round(100 * count($sslv2) / $total["count"]) ?>%</span></td>
-				<td style="width: 50%;">
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="<?= count($sslv2) ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * count($sslv2) / $total["count"] ?>%"></div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>SSL 3</td>
-                <td><?= $sslv3["count"] ?> <span class="text-muted"><?= round(100 * $sslv3["count"] / $total["count"]) ?>%</span></td>
-				<td>
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="<?= $sslv3["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $sslv3["count"] / $total["count"] ?>%"></div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>TLS 1.0</td>
-                <td><?= $tlsv1["count"] ?> <span class="text-muted"><?= round(100 * $tlsv1["count"] / $total["count"]) ?>%</span></td>
-				<td>
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="<?= $tlsv1["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $tlsv1["count"] / $total["count"] ?>%"></div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>TLS 1.1</td>
-                <td><?= $tlsv1_1["count"] ?> <span class="text-muted"><?= round(100 * $tlsv1_1["count"] / $total["count"]) ?>%</span></td>
-				<td>
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="<?= $tlsv1_1["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $tlsv1_1["count"] / $total["count"] ?>%"></div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>TLS 1.2</td>
-                <td><?= $tlsv1_2["count"] ?> <span class="text-muted"><?= round(100 * $tlsv1_2["count"] / $total["count"]) ?>%</span></td>
-				<td>
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="<?= $tlsv1_2["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $tlsv1_2["count"] / $total["count"] ?>%"></div>
-					</div>
-				</td>
-			</tr>
-		</table>
+				<h1>Various reports of all servers tested</h1>
 
-		<h3 id="grades">Grades <small class="text-muted"><?= $total["count"] ?> results</small></h3>
+		        <h3 id="tls">TLS versions <small class="text-muted"><?= $total["count"] ?> results</small></h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>A</th>
-				<td><?= $score_A["count"] ?> <span class="text-muted"><?= round(100 * $score_A["count"] / $total["count"]) ?>%</span></td>
-			</tr>
-			<tr>
-				<th>B</th>
-				<td><?= $score_B["count"] ?> <span class="text-muted"><?= round(100 * $score_B["count"] / $total["count"]) ?>%</span></td>
-			</tr>
-			<tr>
-				<th>C</th>
-				<td><?= $score_C["count"] ?> <span class="text-muted"><?= round(100 * $score_C["count"] / $total["count"]) ?>%</span></td>
-			</tr>
-			<tr>
-				<th>D</th>
-				<td><?= $score_D["count"] ?> <span class="text-muted"><?= round(100 * $score_D["count"] / $total["count"]) ?>%</span></td>
-			</tr>
-			<tr>
-				<th>E</th>
-				<td><?= $score_E["count"] ?> <span class="text-muted"><?= round(100 * $score_E["count"] / $total["count"]) ?>%</span></td>
-			</tr>
-			<tr>
-				<th>F</th>
-				<td><?= $score_F["count"] ?> <span class="text-muted"><?= round(100 * $score_F["count"] / $total["count"]) ?>%</span></td>
-			</tr>
-		</table>
+				<table class="table table-bordered table-striped">
+					<tr>
+		                <td>SSL 2</td>
+		                <td><?= count($sslv2) ?> <span class="text-muted"><?= round(100 * count($sslv2) / $total["count"]) ?>%</span></td>
+						<td style="width: 50%;">
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="<?= count($sslv2) ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * count($sslv2) / $total["count"] ?>%"></div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>SSL 3</td>
+		                <td><?= $sslv3["count"] ?> <span class="text-muted"><?= round(100 * $sslv3["count"] / $total["count"]) ?>%</span></td>
+						<td>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="<?= $sslv3["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $sslv3["count"] / $total["count"] ?>%"></div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>TLS 1.0</td>
+		                <td><?= $tlsv1["count"] ?> <span class="text-muted"><?= round(100 * $tlsv1["count"] / $total["count"]) ?>%</span></td>
+						<td>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="<?= $tlsv1["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $tlsv1["count"] / $total["count"] ?>%"></div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>TLS 1.1</td>
+		                <td><?= $tlsv1_1["count"] ?> <span class="text-muted"><?= round(100 * $tlsv1_1["count"] / $total["count"]) ?>%</span></td>
+						<td>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="<?= $tlsv1_1["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $tlsv1_1["count"] / $total["count"] ?>%"></div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>TLS 1.2</td>
+		                <td><?= $tlsv1_2["count"] ?> <span class="text-muted"><?= round(100 * $tlsv1_2["count"] / $total["count"]) ?>%</span></td>
+						<td>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="<?= $tlsv1_2["count"] ?>" aria-valuemin="0" aria-valuemax="<?= $total["count"] ?>" style="width: <?= 100 * $tlsv1_2["count"] / $total["count"] ?>%"></div>
+							</div>
+						</td>
+					</tr>
+				</table>
 
-		<span class="text-muted">Does not penalize untrusted certificates or SSLv2 support.</span>
+				<h3 id="grades">Grades <small class="text-muted"><?= $total["count"] ?> results</small></h3>
 
-		<h3 id="rsa">RSA key sizes for domain certificates</h3>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>A</th>
+						<td><?= $score_A["count"] ?> <span class="text-muted"><?= round(100 * $score_A["count"] / $total["count"]) ?>%</span></td>
+					</tr>
+					<tr>
+						<th>B</th>
+						<td><?= $score_B["count"] ?> <span class="text-muted"><?= round(100 * $score_B["count"] / $total["count"]) ?>%</span></td>
+					</tr>
+					<tr>
+						<th>C</th>
+						<td><?= $score_C["count"] ?> <span class="text-muted"><?= round(100 * $score_C["count"] / $total["count"]) ?>%</span></td>
+					</tr>
+					<tr>
+						<th>D</th>
+						<td><?= $score_D["count"] ?> <span class="text-muted"><?= round(100 * $score_D["count"] / $total["count"]) ?>%</span></td>
+					</tr>
+					<tr>
+						<th>E</th>
+						<td><?= $score_E["count"] ?> <span class="text-muted"><?= round(100 * $score_E["count"] / $total["count"]) ?>%</span></td>
+					</tr>
+					<tr>
+						<th>F</th>
+						<td><?= $score_F["count"] ?> <span class="text-muted"><?= round(100 * $score_F["count"] / $total["count"]) ?>%</span></td>
+					</tr>
+				</table>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>RSA key size</th>
-				<th>Count</th>
-			</tr>
+				<span class="text-muted">Does not penalize untrusted certificates or SSLv2 support.</span>
+
+				<h3 id="rsa">RSA key sizes for domain certificates</h3>
+
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>RSA key size</th>
+						<th>Count</th>
+					</tr>
 <?php
 $total = 0;
 
@@ -274,36 +286,36 @@ foreach ($bitsizes as $bitsize) {
 
 foreach ($bitsizes as $bitsize) {
 ?>
-			<tr>
-				<td><?= $bitsize["rsa_bitsize"] ?></td>
-                <td><?= $bitsize["count"] ?> <span class="text-muted"><?= round(100 * $bitsize["count"] / $total) ?>%</span></td>
-			</tr>
+					<tr>
+						<td><?= $bitsize["rsa_bitsize"] ?></td>
+		                <td><?= $bitsize["count"] ?> <span class="text-muted"><?= round(100 * $bitsize["count"] / $total) ?>%</span></td>
+					</tr>
 <?php
 }
 ?>
-		</table>
+				</table>
 
-		<h3 id="starttls">StartTLS</h3>
+				<h3 id="starttls">StartTLS</h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>Type</th>
-				<th>Required</th>
-				<th>Allowed</th>
-			</tr>
-			<tr>
-				<td>Client to server</td>
-                <td><?= $c2s_starttls_required["count"] ?> <span class="text-muted"><?= round(100 * $c2s_starttls_required["count"] / ($c2s_starttls_required["count"] + $c2s_starttls_allowed["count"])) ?>%</span></td>
-                <td><?= $c2s_starttls_allowed["count"] ?> <span class="text-muted"><?= round(100 * $c2s_starttls_allowed["count"] / ($c2s_starttls_required["count"] + $c2s_starttls_allowed["count"])) ?>%</span></td>
-			</tr>
-			<tr>
-				<td>Server to server</td>
-                <td><?= $s2s_starttls_required["count"] ?> <span class="text-muted"><?= round(100 * $s2s_starttls_required["count"] / ($s2s_starttls_required["count"] + $s2s_starttls_allowed["count"])) ?>%</span></td>
-                <td><?= $s2s_starttls_allowed["count"] ?> <span class="text-muted"><?= round(100 * $s2s_starttls_allowed["count"] / ($s2s_starttls_required["count"] + $s2s_starttls_allowed["count"])) ?>%</span></td>
-			</tr>
-		</table>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Type</th>
+						<th>Required</th>
+						<th>Allowed</th>
+					</tr>
+					<tr>
+						<td>Client to server</td>
+		                <td><?= $c2s_starttls_required["count"] ?> <span class="text-muted"><?= round(100 * $c2s_starttls_required["count"] / ($c2s_starttls_required["count"] + $c2s_starttls_allowed["count"])) ?>%</span></td>
+		                <td><?= $c2s_starttls_allowed["count"] ?> <span class="text-muted"><?= round(100 * $c2s_starttls_allowed["count"] / ($c2s_starttls_required["count"] + $c2s_starttls_allowed["count"])) ?>%</span></td>
+					</tr>
+					<tr>
+						<td>Server to server</td>
+		                <td><?= $s2s_starttls_required["count"] ?> <span class="text-muted"><?= round(100 * $s2s_starttls_required["count"] / ($s2s_starttls_required["count"] + $s2s_starttls_allowed["count"])) ?>%</span></td>
+		                <td><?= $s2s_starttls_allowed["count"] ?> <span class="text-muted"><?= round(100 * $s2s_starttls_allowed["count"] / ($s2s_starttls_required["count"] + $s2s_starttls_allowed["count"])) ?>%</span></td>
+					</tr>
+				</table>
 
-		<h3 id="trust">Trust</h3>
+				<h3 id="trust">Trust</h3>
 
 <?php
 
@@ -311,128 +323,130 @@ $total = $trusted_valid[0]["count"] + $trusted_valid[1]["count"] + $trusted_vali
 
 ?>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th></th>
-				<th>Trusted</th>
-				<th>Untrusted</th>
-			</tr>
-			<tr>
-				<th>Valid</td>
-				<td><?= $trusted_valid[3]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[3]["count"] / $total) ?>%</span></td>
-				<td><?= $trusted_valid[1]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[1]["count"] / $total) ?>%</span></td>
-			</tr>
-			<tr>
-				<th>Invalid</td>
-				<td><?= $trusted_valid[2]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[2]["count"] / $total) ?>%</span></td>
-				<td><?= $trusted_valid[0]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[0]["count"] / $total) ?>%</span></td>
-			</tr>
-		</table>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th></th>
+						<th>Trusted</th>
+						<th>Untrusted</th>
+					</tr>
+					<tr>
+						<th>Valid</td>
+						<td><?= $trusted_valid[3]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[3]["count"] / $total) ?>%</span></td>
+						<td><?= $trusted_valid[1]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[1]["count"] / $total) ?>%</span></td>
+					</tr>
+					<tr>
+						<th>Invalid</td>
+						<td><?= $trusted_valid[2]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[2]["count"] / $total) ?>%</span></td>
+						<td><?= $trusted_valid[0]["count"] ?> <span class="text-muted"><?= round(100 * $trusted_valid[0]["count"] / $total) ?>%</span></td>
+					</tr>
+				</table>
 
-		<h3 id="sslv3butnottls1">Servers supporting SSL 3, but not TLS 1.0 <small class="text-muted"><?= count($sslv3_not_tls1) ?> results</small></h3>
+				<h3 id="sslv3butnottls1">Servers supporting SSL 3, but not TLS 1.0 <small class="text-muted"><?= count($sslv3_not_tls1) ?> results</small></h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>Target</th>
-				<th>Type</th>
-				<th>When</th>
-			</tr>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Target</th>
+						<th>Type</th>
+						<th>When</th>
+					</tr>
 <?php
 foreach ($sslv3_not_tls1 as $result) {
 ?>
-			<tr>
-				<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
-				<td><?= $result["type"] ?> to server</td>
-				<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
-			</tr>
+					<tr>
+						<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
+						<td><?= $result["type"] ?> to server</td>
+						<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
+					</tr>
 <?php
 }
 ?>
-		</table>
+				</table>
 
-		<h3 id="sslv2wallofshame">Servers supporting SSL 2 <small class="text-muted"><?= count($sslv2) ?> results</small></h3>
+				<h3 id="sslv2wallofshame">Servers supporting SSL 2 <small class="text-muted"><?= count($sslv2) ?> results</small></h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>Target</th>
-				<th>Type</th>
-				<th>When</th>
-			</tr>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Target</th>
+						<th>Type</th>
+						<th>When</th>
+					</tr>
 <?php
 foreach ($sslv2 as $result) {
 ?>
-			<tr>
-				<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
-				<td><?= $result["type"] ?> to server</td>
-				<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
-			</tr>
+					<tr>
+						<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
+						<td><?= $result["type"] ?> to server</td>
+						<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
+					</tr>
 <?php
 }
 ?>
-		</table>
+				</table>
 
-		<h3 id="dnssecsrv">Servers with DNSSEC signed SRV records <small class="text-muted"><?= count($dnssec_srv) ?> results</small></h3>
+				<h3 id="dnssecsrv">Servers with DNSSEC signed SRV records <small class="text-muted"><?= count($dnssec_srv) ?> results</small></h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>Target</th>
-				<th>Type</th>
-				<th>When</th>
-			</tr>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Target</th>
+						<th>Type</th>
+						<th>When</th>
+					</tr>
 <?php
 foreach ($dnssec_srv as $result) {
 ?>
-			<tr>
-				<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
-				<td><?= $result["type"] ?> to server</td>
-				<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
-			</tr>
+					<tr>
+						<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
+						<td><?= $result["type"] ?> to server</td>
+						<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
+					</tr>
 <?php
 }
 ?>
-		</table>
+				</table>
 
-		<h3 id="reordersciphers">Servers that pick their own cipher order <small class="text-muted"><?= count($reorders_ciphers) ?> results</small></h3>
+				<h3 id="reordersciphers">Servers that pick their own cipher order <small class="text-muted"><?= count($reorders_ciphers) ?> results</small></h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>Target</th>
-				<th>Type</th>
-				<th>When</th>
-			</tr>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Target</th>
+						<th>Type</th>
+						<th>When</th>
+					</tr>
 <?php
 foreach ($reorders_ciphers as $result) {
 ?>
-			<tr>
-				<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
-				<td><?= $result["type"] ?> to server</td>
-				<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
-			</tr>
+					<tr>
+						<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
+						<td><?= $result["type"] ?> to server</td>
+						<td><time class="timeago" datetime="<?= date("c", strtotime($result["test_date"])) ?>"><?= date("c", strtotime($result["test_date"])) ?></time></td>
+					</tr>
 <?php
 }
 ?>
-		</table>
+				</table>
 
-		<h3 id="sharesprivatekeys">Servers sharing private keys <small class="text-muted"><?= count($shares_private_keys) ?> results</small></h3>
+				<h3 id="sharesprivatekeys">Servers sharing private keys <small class="text-muted"><?= count($shares_private_keys) ?> results</small></h3>
 
-		<table class="table table-bordered table-striped">
-			<tr>
-				<th>Target</th>
-				<th>Type</th>
-				<th>SHA256(SPKI)</th>
-			</tr>
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Target</th>
+						<th>Type</th>
+						<th>SHA256(SPKI)</th>
+					</tr>
 <?php
 foreach ($shares_private_keys as $result) {
 ?>
-			<tr>
-				<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
-				<td><?= $result["type"] ?> to server</td>
-				<td><?= fp($result["subject_key_info_sha256"]) ?></td>
-			</tr>
+					<tr>
+						<td><a href="result.php?domain=<?= $result["server_name"] ?>&amp;type=<?= $result["type"] ?>"><?= $result["server_name"] ?></a></td>
+						<td><?= $result["type"] ?> to server</td>
+						<td><?= fp($result["subject_key_info_sha256"]) ?></td>
+					</tr>
 <?php
 }
 ?>
-		</table>
+				</table>
+			</div>
+		</div>
 
 		
 		<div class="footer">
