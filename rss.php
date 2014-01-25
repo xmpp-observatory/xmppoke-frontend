808d1faa7fca370781d3e1f0919725e63fce76f6
Thijs Alkemade <me@thijsalkema.de>
2014-01-25 18:53:11 +0100

    Fix the cdata, add the title of the post to index.php.

--- rss.php
+++ rss.php
@@ -26,7 +26,7 @@ foreach ($news as $new) {
 ?>
 <item>
 		<title><?= $new["title"] ?></title>
-		<description><![CDATA[[[<?= $new["message"] ?>]]></description>
+		<description><![CDATA[<?= $new["message"] ?>]]></description>
 		<link>https://xmpp.net/</link>
 		<guid>https://xmpp.net/news.php?id=<?= $new["post_id"] ?></guid>
 		<pubDate><?= date("D, d M Y H:i:s O", strtotime($new["post_date"])) ?></pubDate>
