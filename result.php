fdc1556223c64d16144ffaade86586164743adcd
Thijs Alkemade <me@thijsalkema.de>
2014-08-18 14:42:00 +0200

    Cache old results for 5 min.

--- result.php
+++ result.php
@@ -382,6 +382,7 @@ if ($result) {
 		$refresh = 15;
 	} else {
 		$date = strtotime($result->test_date);
+		header("Cache-Control: must-revalidate, max-age=300");
 		header("Last-Modified: " . gmdate("D, d M Y H:i:s", $date + 30 * 60) . " GMT");
 	}
 
